e8dc75dcff550ea4a1a4b9fc3cea574c15dc9a88
Kiril <kiril.gudim@gmail.com>
2019-12-11 12:57:23 +0200

    Lab 3, exercise 1

--- Exercises/1-random.js
+++ Exercises/1-random.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const random = (min, max) => {
-  // Generate random Number between from min to max
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  max === undefined ?
+    (max = min, min = 0) : (max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 module.exports = { random };
