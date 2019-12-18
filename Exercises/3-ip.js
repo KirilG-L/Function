47b3484b57f82a4cbe897f19bc2b54d66ce1b5c1
Kiril <kiril.gudim@gmail.com>
2019-12-18 21:48:00 +0200

    refactor 3-ip.js

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  const shiftIp = ip.split('.').map(Number);
-  for (let i = 0, j = 3; j >= 0; i++, j--) {
-    shiftIp[i] <<= 8 * j;
-  }
-  const sumIp = shiftIp.reduce((acc, cur) => acc + cur);
+  const sum = (acc, cur) => (acc << 8) + cur;
+  return ip.split('.').map(Number).reduce(sum, 0);
 };
 
 module.exports = { ipToInt };
